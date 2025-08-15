9d80ff7c13dbb057c39fb951b2e3423b30a7cb41
Mark Allen <63212409+808cn163@users.noreply.github.com>
2025-08-15 16:19:05 +0800

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,14 +1,11 @@
 export const DEFAULT_MODEL = "openai/gpt-oss-120b";
 
 export const SUPPORTED_MODELS = [
-  "anthropic/claude-sonnet-4",
-  "anthropic/claude-opus-4.1",
   "openai/gpt-5",
   "openai/o3",
   "openai/gpt-5-mini ",
   "openai/gpt-oss-120b",
   "openai/gpt-4o-mini",
   "openai/gpt-5-nano",
-  "xai/grok-4",
-  "xai/grok-3",
+  "anthropic/claude-sonnet-4",
 ];
